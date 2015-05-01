9407978d7844887b66130eb76e7facb1d87fd456
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-05-01 15:14:50 +0100

    added set and get method for process id. This way we can pass process id to consumer object so they can lock jobs

--- src/QueueZilla/Framework/Queue/QueueConsumer.php
+++ src/QueueZilla/Framework/Queue/QueueConsumer.php
@@ -7,6 +7,13 @@ namespace QueueZilla\Framework\Queue;
 
 abstract class QueueConsumer implements IQueueConsumer
 {
+  /**
+   * Identifier string for process. This is used for locking jobs from a queue
+   *
+   * @var string $_processId
+   */
+  protected $_processId;
+
   public function consume()
   {
     while(true)
@@ -26,6 +33,15 @@ abstract class QueueConsumer implements IQueueConsumer
     }
   }
 
+  public function setProcessId($id)
+  {
+    $this->_processId = $id;
+  }
+
+  protected function _getProcessId()
+  {
+    return $this->_processId;
+  }
 
   public function takeABreak()
   {
